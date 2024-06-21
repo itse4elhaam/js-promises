d3945fe02557961bffd1d3e567df857907de3494
Elhaam <e4elhaam@gmail.com>
2024-06-22 02:34:27 +0500

    feat: all funcs completed

--- MyPromise.js
+++ MyPromise.js
@@ -7,7 +7,7 @@ const STATE = {
 class MyPromise {
 	#thenCbs = [];
 	#catchCbs = [];
-	#state; // will be used to track current status on the promise
+	#state = STATE.PENDING; // will be used to track current status on the promise
 	#value;
 	#onSuccessBind = this.#onSuccess.bind(this);
 	#onFailBind = this.#onFail.bind(this);
@@ -35,16 +35,32 @@ class MyPromise {
 	}
 	// these are private because we only expose .then .catch . finally
 	#onSuccess(value) {
-		if (this.#state !== STATE.PENDING) return;
-		this.#value = value;
-		this.#state = STATE.FULFILLED;
-		this.#runCallbacks();
+		queueMicrotask(() => {
+			if (this.#state !== STATE.PENDING) return;
+			if (value instanceof MyPromise) {
+				value.then(this.#onSuccessBind, this.#onFailBind);
+				return;
+			}
+			this.#value = value;
+			this.#state = STATE.FULFILLED;
+			this.#runCallbacks();
+		})
 	}
 	#onFail(value) {
-		if (this.#state !== STATE.PENDING) return;
-		this.#value = value;
-		this.#state = STATE.REJECTED;
-		this.#runCallbacks();
+		queueMicrotask(() => {
+			if (this.#state !== STATE.PENDING) return;
+			if (value instanceof MyPromise) {
+				value.then(this.#onSuccessBind, this.#onFailBind);
+				return;
+			}
+
+			if (this.#catchCbs.length === 0) {
+				throw new UncaughtPromiseError(value)
+			}
+			this.#value = value;
+			this.#state = STATE.REJECTED;
+			this.#runCallbacks();
+		})
 	}
 
 	then(thenCb, catchCb) {
@@ -57,7 +73,7 @@ class MyPromise {
 				try {
 					resolve(thenCb(result));
 				} catch (error) {
-					resolve(error)
+					reject(error)
 				}
 			})
 			this.#catchCbs.push(result => {
@@ -68,18 +84,106 @@ class MyPromise {
 				try {
 					resolve(catchCb(result));
 				} catch (error) {
-					resolve(error)
+					reject(error);
 				}
 			})
 			this.#runCallbacks();
 		})
 	}
 	catch(cb) {
-		this.then(null, cb);
+		return this.then(null, cb);
 	}
-	finally(cn) {
+	finally(cb) {
+		return this.then(result => {
+			cb();
+			return result;
+		}, result => {
+			cb();
+			throw result;
+		})
+	}
+	static resolve(value) {
+		return new Promise(resolve => {
+			resolve(value)
+		})
+	}
 
+	static reject(value) {
+		return new Promise((resolve, reject) => {
+			reject(value)
+		})
+	}
+
+	static all(promises) {
+		const results = [];
+		let completedPromises = 0;
+		return new MyPromise((resolve, reject) => {
+			for (let i = 0; i < promises.length; i++) {
+				const promise = promises[i];
+				promise.then(value => {
+					completedPromises++;
+					results[i] = value;
+					if (completedPromises === promises.length) {
+						resolve(results);
+					}
+				}).catch(reject)
+			}
+		})
+	}
+	static allSettled(promises) {
+		const results = []
+		let completedPromises = 0
+		return new MyPromise(resolve => {
+			for (let i = 0; i < promises.length; i++) {
+				const promise = promises[i]
+				promise
+					.then(value => {
+						results[i] = { status: STATE.FULFILLED, value }
+					})
+					.catch(reason => {
+						results[i] = { status: STATE.REJECTED, reason }
+					})
+					.finally(() => {
+						completedPromises++
+						if (completedPromises === promises.length) {
+							resolve(results)
+						}
+					})
+			}
+		})
+	}
+	static race(promises) {
+		return new MyPromise((resolve, reject) => {
+			promises.forEach(promise => {
+				promise.then(resolve).catch(reject);
+			})
+		})
+	}
+	static any(promises) {
+		const errors = [];
+		let erredPromises = 0;
+		return new MyPromise((resolve, reject) => {
+			for (let i = 0; i < promises.length; i++) {
+				const promise = promises[i];
+				promise.then(resolve).reject(value => {
+					erredPromises++;
+					errors[i] = value;
+					if (erredPromises === promises.length) {
+						reject(new AggregateError(errors, "All promises were rejected"))
+					}
+				})
+			}
+		})
+	}
+
+}
+
+class UncaughtPromiseError extends Error {
+	constructor(error) {
+		super(error);
+		this.stack = `(in promise) ${error.stack}`
 	}
 }
+
 
 module.exports = MyPromise;
